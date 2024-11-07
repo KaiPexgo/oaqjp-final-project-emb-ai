1b3c9953df16a179905ca0ab824f7369c5ebfa55
Christopher Aguilar <kyleaguilar2@gmail.com>
2024-11-07 16:36:06 -0500

    lab complete

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,9 +14,19 @@ def emotion_detector(text_to_analyse):
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
 
-    # Parse the response from the API
-    formatted_response = json.loads(response.text)["emotionPredictions"][0]["emotion"]
-    formatted_response["dominant_emotion"] = max(formatted_response, key=formatted_response.get)
-
-    # Return the label and score in a dictionary
-    return formatted_response
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)["emotionPredictions"][0]["emotion"]
+        formatted_response["dominant_emotion"] = max(formatted_response, key=formatted_response.get)
+        return formatted_response
+    # If the response status code is 400, set label and score to None
+    elif response.status_code == 500:
+        return {
+            "anger": None, 
+            "disgust": None, 
+            "fear": None, 
+            "joy": None, 
+            "sadness": None, 
+            "dominant_emotion": None
+        }
